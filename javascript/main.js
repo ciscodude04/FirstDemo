b4156a81c10d189782e61db69890e7544882f2bf
Cisco <fjdiaz2k11@hotmail.com>
2017-03-24 21:36:41 -0700

    Add pages and main core

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,21 @@ function randomOption(colors) {
     "use strict";
     var randomColor = Math.random(0, colors.length);
 }
+
+
+var msrp = document.getElementById('#MSRP').innerHTML;
+var tax = document.getElementById('#TERM').innerHTML;
+var term = document.getElementById('#APR').innerHTML;
+var apr = document.getElementById('#TAX').innerHTML;
+var downPayment = document.getElementById('#DOWNPAYMENT').innerHTML;
+var tradeIn = document.getElementById('#TRADEIN').innerHTML;
+
+function monthlyPayments() {
+    var loanAmount = msrp - (downPayment - tradeIn);
+    var taxTotal = loanAmount * tax;
+    var aprTotal = loanAmount * apr;
+    var totalLoanAmount = loanAmount + taxTotal + aprTotal;
+    return totalLoanAmount;
+}
+
+monthlyPayments();
